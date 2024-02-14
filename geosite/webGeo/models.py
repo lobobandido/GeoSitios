2a3a2bb304440a4baaf6d18ce084633f9b3a017b
lobobandido <halcon.chiva@gmail.com>
2024-02-13 23:07:32 -0600

    lsito conectado el admin y superuser

--- geosite/webGeo/models.py
+++ geosite/webGeo/models.py
@@ -7,7 +7,11 @@ class Sitio(models.Model):
     latitud = models.DecimalField(max_digits=9, decimal_places=6)
     longitud = models.DecimalField(max_digits=9, decimal_places=6)
     direccion = models.CharField(max_length=255)
-    #fecha_registro = models.DateTimeField(auto_now_add=True)
+    imagen = models.ImageField(upload_to='sitio', blank=True)
+    fecha_registro = models.DateTimeField(auto_now_add=True)
+    #fecha_creacion = models.DateTimeField(default=timezone.now)
+    
+    
 
     def __str__(self):
         return self.nombre
@@ -16,8 +20,9 @@ class OrdenServicio(models.Model):
     numero = models.CharField(max_length=20)
     sitio = models.ForeignKey(Sitio, on_delete=models.RESTRICT)
     ingeniero_asig= models.TextField(null=True) 
-    #fecha_registro = models.DateTimeField(auto_now_add=True)
-    # Otros campos relevantes para la orden de servicio
+    #fecha_creacion = models.DateTimeField(default=timezone.now)
+    fecha_registro = models.DateTimeField(auto_now_add=True)
+    # Otros campos relevantes para la orden de servicio conforme avance definire q campos agregar 
 
     def __str__(self):
         return f'Orden de Servicio #{self.numero}'
